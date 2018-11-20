14b1b194d5e902e30303f636bd8664e20760c2c0
Romariobispo <romario.silva@dcomp.ufs.br>
2018-11-20 16:26:06 +0000

    radio amador fixes

--- src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP1.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP1.java
@@ -1,19 +1,23 @@
 package br.ufs.dcomp.ExemploUdpJava;
 import java.util.*;
 import java.net.*;
+import java.lang.*;
 
 public class AppUDP1 {
 
     public static void main(String[] args) throws SocketException {
         try{
+            while (true){
+                
             System.out.print("[ Alocando porta UDP      ..................  ");
     	    DatagramSocket socket = new DatagramSocket(10000);
             System.out.println("[OK] ]");
-            System.out.println("insira a msg")
+            System.out.println("insira a msg");
             Scanner sc = new Scanner (System.in);
             String msg = sc.nextLine();
-            //String msg = "Olá!!!";
-            
+            if (msg == "cambio"){
+                System.exit(0);
+            }
             byte[] msg_buf = msg.getBytes();
             int msg_size = msg_buf.length;
             InetAddress destination_address = InetAddress.getLocalHost();
@@ -26,7 +30,34 @@ public class AppUDP1 {
             System.out.print("[ Enviando datagrama UDP  ..................  ");
             socket.send(pack);
             System.out.println("[OK] ]");
+            
+            // Parte do recebimento da msg
+            
+               
+            System.out.print("[ Alocando porta UDP                  ..................  ");
+            System.out.println("[OK] ]");
 
+            byte[] buf = new byte[20]; // Este vetor de bytes vai ser usado para o pacote
+            DatagramPacket pack1 = new DatagramPacket(buf, buf.length); // criação de um pacote vazio
+
+            System.out.print("[ Aguardando recebimento de mensagem  ..................  ");
+            socket.receive(pack1); //operação bloqueante (enquanto não chegar nada, esse processo ficará esperando)
+            System.out.println("[OK] ]");
+            
+            byte[] received_data = pack1.getData();
+            String received_msg = new String(received_data); 
+            InetAddress origin_address = pack1.getAddress();
+            int origin_port = pack1.getPort();
+            if (received_msg == "cambio"){
+               System.exit(0);
+            }
+            System.out.println("  Mensagem:             "+received_msg);
+            System.out.println("  Endereço de origem:   "+origin_address.getHostAddress());
+            System.out.println("  Porta de origem:      "+origin_port);
+            
+            // Parte do recebimento da msg
+            socket.close();
+            }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }    
--- src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP2.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP2.java
@@ -1,7 +1,8 @@
 package br.ufs.dcomp.ExemploUdpJava; 
 
 import java.net.*;
-
+import java.util.*;
+import java.lang.*;
 /*
     Alguns comandos úteis:
             cd Pasta do projeto. e.g cd ExemploUdpjava
@@ -13,9 +14,7 @@ public class AppUDP2 {
 
     public static void main(String[] args) throws SocketException {
         try{
-
-            
-            
+            while(true){
             System.out.print("[ Alocando porta UDP                  ..................  ");
     	    DatagramSocket socket = new DatagramSocket(20000);
             System.out.println("[OK] ]");
@@ -32,10 +31,42 @@ public class AppUDP2 {
             InetAddress origin_address = pack.getAddress();
             int origin_port = pack.getPort();
             
+            if (received_msg == "cambio"){
+                System.exit(0);
+            }
+            
             System.out.println("  Mensagem:             "+received_msg);
             System.out.println("  Endereço de origem:   "+origin_address.getHostAddress());
             System.out.println("  Porta de origem:      "+origin_port);
         
+               // Parte do envio de msg
+                
+            System.out.print("[ Alocando porta UDP      ..................  ");
+            System.out.println("[OK] ]");
+            System.out.println("insira a msg");
+            Scanner sc = new Scanner (System.in);
+            String msg = sc.nextLine();
+            
+            byte[] msg_buf = msg.getBytes();
+            int msg_size = msg_buf.length;
+            InetAddress destination_address = InetAddress.getLocalHost();
+            int destination_port = 10000; 
+
+            System.out.print("[ Montando datagrama UDP  ..................  ");
+            DatagramPacket pack1 = new DatagramPacket(msg_buf, msg_size, destination_address, destination_port);
+            System.out.println("[OK] ]");
+            
+            if (msg == "cambio"){
+                System.exit(0);
+            }
+            
+            System.out.print("[ Enviando datagrama UDP  ..................  ");
+            socket.send(pack1);
+            System.out.println("[OK] ]");
+            
+                // Parte do envio de msg
+            socket.close();    
+            }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }    
